0e790cd8a1dfa9a9782b69fd55464b741a997653
Ghost <rildiarov@gmail.com>
2024-09-02 17:16:42 +0400

    add mobile screens

--- SchoolFront/lib/screens/main_page/home_screen_body.dart
+++ SchoolFront/lib/screens/main_page/home_screen_body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:school/dataclasses/review.dart';
 import 'package:school/screens/main_page/widgets/home_screen_widgets.dart';
 
-
 class HomeScreenBody extends StatelessWidget {
   final String profileImageUrl;
   final List<Review> reviews;
@@ -12,7 +11,6 @@ class HomeScreenBody extends StatelessWidget {
     required this.profileImageUrl,
     required this.reviews,
   }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -24,17 +22,33 @@ class HomeScreenBody extends StatelessWidget {
           end: Alignment.bottomCenter,
         ),
       ),
-      child: SingleChildScrollView(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(child: buildInfoCard()),
-            const SizedBox(width: 20),
-            Expanded(child: buildProfileAndReviews(context, profileImageUrl, reviews)),
-          ],
-        ),
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          bool isMobile = constraints.maxWidth < 600; // Adjust threshold as needed
+
+          return SingleChildScrollView(
+            child: isMobile
+                ? Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                buildInfoCard(),
+                const SizedBox(height: 20),
+                buildProfileAndReviews(context, profileImageUrl, reviews),
+              ],
+            )
+                : Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(child: buildInfoCard()),
+                const SizedBox(width: 20),
+                Expanded(child: buildProfileAndReviews(context, profileImageUrl, reviews)),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
+
 }
